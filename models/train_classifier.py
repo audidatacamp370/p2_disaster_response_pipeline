5415b124dbb5fe527d07010a14ade69bee9059ac
grinsecat <grinsecat@gmx.de>
2023-06-30 15:48:56 +0200

    Modified train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,103 @@
 import sys
+import re
+import numpy as np
+import pandas as pd
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
 
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger', 'stopwords'])
+
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import confusion_matrix, classification_report, make_scorer, accuracy_score, f1_score, fbeta_score
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+import pickle
 
 def load_data(database_filepath):
-    pass
-
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql_table(database_filepath, engine)
+    X = df.message.values
+    y = df.iloc[:,4:]
+    return X, y
 
 def tokenize(text):
-    pass
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    
+    detected_urls = re.findall(url_regex, text)
+    
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    text_norm=text.lower().strip()
+
+    tokens = word_tokenize(text_norm) 
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = WordNetLemmatizer().lemmatize(tok)
+        if clean_tok not in stopwords.words("english"):
+            clean_tokens.append(clean_tok)
+    
+    return clean_tokens
+
+class StartingVerbExtractor(BaseEstimator, TransformerMixin):
+
+    def starting_verb(self, text):
+        sentence_list = nltk.sent_tokenize(text)
+        for sentence in sentence_list:
+            pos_tags = nltk.pos_tag(tokenize(sentence))
+            first_word, first_tag = pos_tags[0]
+            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
+                return True
+        return False
+
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, X):
+        X_tagged = pd.Series(X).apply(self.starting_verb)
+        return pd.DataFrame(X_tagged)
 
 
 def build_model():
-    pass
+    pipeline=Pipeline([
+        ('features', FeatureUnion([
 
+            ('text_pipeline', Pipeline([
+                ('vect', CountVectorizer(tokenizer=tokenize)),
+                ('tfidf', TfidfTransformer())
+            ])),
+
+            ('starting_verb', StartingVerbExtractor())
+        ])),
+
+        ('clf', MultiOutputClassifier(AdaBoostClassifier(), n_jobs=-1))
+    ])
+        
+    
+    parameters = {
+        'clf__estimator__base_estimator': [None, 'RandomForestClassifier'],
+        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__learning_rate': [0.5, 1, 2]
+        }
+
+    model = GridSearchCV(pipeline, param_grid=parameters, cv=2, n_jobs=-1)
+
+    return model
 
 def evaluate_model(model, X_test, Y_test, category_names):
     pass
 
 
 def save_model(model, model_filepath):
-    pass
+    pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
